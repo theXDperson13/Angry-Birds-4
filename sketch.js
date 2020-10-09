aa25f97dec0c5072771cfb504c43bb873793b8e8
Prasad Sankaran <prasadsankaran@Prasads-MacBook-Pro.local>
2020-10-08 20:51:50 -0600

    created contraint

--- sketch.js
+++ sketch.js
@@ -35,6 +35,8 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    log6 = new Log(200,100,50,PI/2);
+    chain = new Chain(bird.body,log6.body);
 
 }
 
@@ -61,4 +63,7 @@ function draw(){
 
     bird.display();
     platform.display();
+    log6.display();
+    chain.connectivity();
+
 }
